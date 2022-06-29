600cfae37b6069ed9c5701e8494d86602dc73c6a
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-29 09:58:04 -0400

    updates to graph ql schema

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,17 +1,59 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   NftMarketplace,
-  ItemBought,
-  ItemDeleted,
-  ItemListed,
-  ItemUpdated
+  ItemBought as ItemBoughtEvent,
+  ItemDeleted as ItemDeletedEvent,
+  ItemListed as ItemListedEvent,
+  ItemUpdated as ItemUpdatedEvent
 } from "../generated/NftMarketplace/NftMarketplace"
-import { ExampleEntity } from "../generated/schema"
 
-export function handleItemBought(event: ItemBought): void {}
+import { ItemListed, ActiveItem, ItemBought, ItemCancelled } from "../generated/schema"
 
-export function handleItemDeleted(event: ItemDeleted): void {}
+export function handleItemBought(event: ItemBoughtEvent): void {
+  // Save that event in our graph
+  // Update our active items
 
-export function handleItemListed(event: ItemListed): void {}
+  // get or create an itemListed object
+  // each item needs a unique Id
 
-export function handleItemUpdated(event: ItemUpdated): void {}
+  // ItemBoughtEvent: Just the raw event
+  // ItemBoughtObject: What we save
+  let itemBought = ItemBought.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+
+  if (!itemBought) {
+    itemBought = new ItemBought(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  }
+
+  itemBought.buyer = event.params.buyer
+  itemBought.nftAddress = event.params.nftAddress
+  itemBought.tokenId = event.params.tokenId
+  activeItem!.buyer = event.params.buyer
+
+  itemBought.save()
+  activeItem!.save()
+}
+
+export const handleItemDeleted = (event: ItemDeletedEvent): void => {}
+
+export const handleItemListed = (event: ItemListedEvent): void => {
+  let itemListed = ItemListed.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  let activeItem = ActiveItem.load(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+
+  if (!itemListed) {
+    itemListed = new ItemListed(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  }
+
+  if (!activeItem) {
+    activeItem = new ActiveItem(getIdFromEventParams(event.params.tokenId, event.params.nftAddress))
+  }
+
+  itemListed.seller = event.params.seller
+  activeItem.seller = event.params.seller
+}
+
+export const handleItemUpdated = (event: ItemUpdatedEvent): void => {}
+
+const getIdFromEventParams = (tokenID: BigInt, nftAddress: Address): string => {
+  return tokenID.toHexString() + nftAddress.toHexString()
+}
